cd7da7d5faa0a83467c32b419a4f505c280d8322
sujal matolia <matoliasujal@gamil.com>
2024-01-26 21:20:06 +0530

    six

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class FeatureDropper(BaseEstimator, TransformerMixin):
         return X.drop(["Embarked", "Name", "Ticket", "Cabin", "Sex", "N" ], axis = 1, errors = "ignore")
     
 
-df = pd.read_csv("test.csv")
+df = pd.read_csv("train.csv")
 test_df = pd.read_csv("test.csv")
 
 split = StratifiedShuffleSplit(n_splits= 1, test_size= 0.2)
